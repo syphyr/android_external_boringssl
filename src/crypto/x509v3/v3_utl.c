5895e888414c4c0794273e3708e631aa38429db1
Matt Caswell <matt@openssl.org>
2021-08-18 17:58:23 +0100

    Fix append_ia5 function to not assume NUL terminated strings
    ASN.1 strings may not be NUL terminated. Don't assume they are.
    
    CVE-2021-3712
    
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>

--- src/crypto/x509v3/v3_utl.c
+++ src/crypto/x509v3/v3_utl.c
@@ -581,13 +581,24 @@ static int append_ia5(STACK_OF(OPENSSL_STRING) **sk, ASN1_IA5STRING *email)
 	char *emtmp;
 	/* First some sanity checks */
 	if(email->type != V_ASN1_IA5STRING) return 1;
-	if(!email->data || !email->length) return 1;
+	if (email->data == NULL || email->length == 0)
+		return 1;
+	if (memchr(email->data, 0, email->length) != NULL)
+		return 1;
 	if(!*sk) *sk = sk_OPENSSL_STRING_new(sk_strcmp);
 	if(!*sk) return 0;
+
+	emtmp = BUF_strndup((char *)email->data, email->length);
+	if (emtmp == NULL)
+		return 0;
+
 	/* Don't add duplicates */
-	if(sk_OPENSSL_STRING_find(*sk, NULL, (char *)email->data)) return 1;
-	emtmp = BUF_strdup((char *)email->data);
-	if(!emtmp || !sk_OPENSSL_STRING_push(*sk, emtmp)) {
+	if(sk_OPENSSL_STRING_find(*sk, NULL, emtmp)) {
+		OPENSSL_free(emtmp);
+		return 1;
+	}
+	if (!sk_OPENSSL_STRING_push(*sk, emtmp)) {
+		OPENSSL_free(emtmp); /* free on push failure */
 		X509_email_free(*sk);
 		*sk = NULL;
 		return 0;
